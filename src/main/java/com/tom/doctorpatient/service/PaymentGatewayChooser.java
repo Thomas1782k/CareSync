c10b75f6c383552df0d12e8027197976ccc687ce
Thomas <thomas1782000t@gmail.com>
2024-07-28 19:32:51 +0530

    small update

--- src/main/java/com/tom/doctorpatient/service/PaymentGatewayChooser.java
+++ src/main/java/com/tom/doctorpatient/service/PaymentGatewayChooser.java
@@ -1,18 +1,15 @@
 package com.tom.doctorpatient.service;
 
 import com.tom.doctorpatient.service.paymentGateway.PaymentGateway;
-import com.tom.doctorpatient.service.paymentGateway.RazorPayGateway;
 import com.tom.doctorpatient.service.paymentGateway.StripeGateway;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentGatewayChooser {
 
-    private RazorPayGateway razorPayGateway;
     private StripeGateway stripeGateway;
 
-    public PaymentGatewayChooser(RazorPayGateway razorPayGateway, StripeGateway stripeGateway) {
-        this.razorPayGateway = razorPayGateway;
+    public PaymentGatewayChooser( StripeGateway stripeGateway) {
         this.stripeGateway = stripeGateway;
     }
 
